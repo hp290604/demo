6273ad06b6400d73c78c489eb9388aaddc096521
Harsh Poddar <hp290604@yahoo.com>
2024-08-27 23:55:11 +0530

    demo folder added

---
+++ src/main/java/H2connectionApplication.java
@@ -0,0 +1,13 @@
+package h2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2connectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2connectionApplication.class, args);
+	}
+
+}
